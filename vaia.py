6f75ff34f8ccf80c113492a5ca0701181869010c
Michael Neeley <micneeley14@gmail.com>
2023-11-13 19:28:51 -0500

    dotenv

--- vaia.py
+++ vaia.py
@@ -1,8 +1,24 @@
-import psycopg2
+import psycopg
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI()
+
+
+def prompt(user_input):
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": user_input},
+        ],
+    )
+    print(response)
 
 
 def add_todo(description, due_date):
-    with psycopg2.connect("dbname=vaia") as conn:
+    with psycopg.connect("dbname=vaia") as conn:
         with conn.cursor() as curs:
             curs.execute(
                 "INSERT INTO todo (description, due_date) VALUES (%s, %s)",
