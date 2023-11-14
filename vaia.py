eb5c7548afe8dbd825b7b4bf769bba252d3ecca5
Michael Neeley <micneeley14@gmail.com>
2023-11-13 20:13:39 -0500

    added main script

--- vaia.py
+++ vaia.py
@@ -1,3 +1,6 @@
+import argparse
+import json
+
 import psycopg
 from dotenv import load_dotenv
 from openai import OpenAI
@@ -7,20 +10,45 @@ load_dotenv()
 client = OpenAI()
 
 
-def prompt(user_input):
+def create_todo(user_input):
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
+        model="gpt-3.5-turbo-1106",
+        response_format={"type": "json_object"},
         messages=[
-            {"role": "user", "content": user_input},
+            {
+                "role": "system",
+                "content": "You are a helpful assistant designed to output JSON.",
+            },
+            {
+                "role": "user",
+                "content": f"""
+                    Take the following user input and turn it into a todo item.
+                    Format as JSON with description and due_date keys.
+                    due_date should be ISO format:
+
+                    {user_input}
+                """,
+            },
         ],
     )
-    print(response)
+    return json.loads(response.choices[0].message.content)
 
 
 def add_todo(description, due_date):
-    with psycopg.connect("dbname=vaia") as conn:
+    print(f"Adding todo: {description} due on {due_date}")
+    exit()
+    with psycopg.connect(DATABASE_URL) as conn:
         with conn.cursor() as curs:
             curs.execute(
                 "INSERT INTO todo (description, due_date) VALUES (%s, %s)",
                 (description, due_date),
             )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Add a todo item")
+    parser.add_argument("prompt", help="Prompt to use for GPT-3")
+    args = parser.parse_args()
+    todo = create_todo(args.prompt)
+    print(todo)
+    add_todo(todo["description"], todo["due_date"])
